520998f290f425b22796215d89e580abe79f87af
chenzh <chenzh@ucweb.com>
2013-08-16 22:48:52 +0800

    fixed project

---
+++ VoiceInput/src/com/example/voiceinput/UserVoice.java
@@ -0,0 +1,11 @@
+
+package com.example.voiceinput;
+
+
+public class UserVoice {
+
+    public String path;
+
+    public String title;
+
+}
---
+++ VoiceInput/src/com/example/voiceinput/UserVoiceModel.java
@@ -0,0 +1,10 @@
+package com.example.voiceinput;
+
+import org.tadpoleframework.model.BaseModel;
+
+public class UserVoiceModel extends BaseModel<UserVoice>  {
+
+    public UserVoiceModel(Class<UserVoice> clazz) {
+        super(clazz);
+    }
+}
